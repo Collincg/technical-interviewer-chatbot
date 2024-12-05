6a6db69c3506f42424fd0b4c73f9bf65ead14953
Collin Graff <graff253@umn.edu>
2024-12-05 13:36:18 +0100

    syntax change

--- streamlitChatWithMemory.py
+++ streamlitChatWithMemory.py
@@ -32,10 +32,10 @@ in the expander below. View the
 
 # use this for StreamLit cloud
 if "GROQ_API_KEY" in st.secrets:
-    GROQ_API_KEY = st.secrets.GROQ_API_KEY
+    GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 else:
     st.info("Enter an Groq API Key to continue")
-    
+
 # Set up memory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
 
